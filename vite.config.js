d1d20929774c54bdd7e83d25d9e619cf66875f1d
oluwatimilehin aramide <aramideoluwatimilehin11@gmail.com>
2026-06-11 22:00:28 -0230

    fix: add relative base path for asset distribution routing

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 🌟 ADD THIS LINE: Tells the browser to use relative paths so the page doesn't load blank!
 })
